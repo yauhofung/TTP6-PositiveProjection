f28650d05e324ddbac3f4bedf0d0d3532c7d910b
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-11 14:49:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// other imports
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class YOUR_COMPONENT extends React.Component {
+	// other member functions and variables can go here
+
+	render() {
+		// JS code goes here
+		// but not _all_ your code
+		const name = 'Jon Chin';
+
+		return (
+			// JSX / HTML  goes here
+			<p>
+				hello {name}
+			</p>
+		)
+	}
 }
 
-export default App;
+export default YOUR_COMPONENT
