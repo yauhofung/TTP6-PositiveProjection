d9376b860b8d8658085dfe8c74b6862134b19bf7
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-12 14:15:17 -0500

    added JSX page anatomy comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// other imports
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -21,6 +23,22 @@ import { Button } from '@material-ui/core'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
+// class YOUR_COMPONENT extends React.Component {
+// 	// other member functions and variables can go here
+
+// 	render() {
+// 		// JS code goes here
+// 		// but not _all_ your code
+// 		const name = 'Jon Chin';
+
+// 		return (
+// 			// JSX / HTML  goes here
+// 			<p>hello {name}</p>
+// 		)
+// 	}
+// }
+
+// export default YOUR_COMPONENT
 
 const drawerWidth = 240;
 
