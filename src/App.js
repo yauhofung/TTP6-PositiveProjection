1090551d166bfc122f9c11a165a6640326368d26
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-12 14:32:09 -0500

    added comments to App.js class

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import { Snackbar } from "@material-ui/core";
 import React, { Component } from "react";
+
+// other imports
+import { Snackbar } from "@material-ui/core";
 import MiniDrawer from './components/MiniDrawer';
 
-
-// class YOUR_COMPONENT extends React.Component {
-// 	// other member functions and variables can go here
-
-// 	render() {
-// 		// JS code goes here
-// 		// but not _all_ your code
-// 		const name = 'Jon Chin';
-
-// 		return (
-// 			// JSX / HTML  goes here
-// 			<p>hello {name}</p>
-// 		)
-// 	}
-// }
-
-// export default YOUR_COMPONENT
-
 class App extends Component {
-  add(a, b) {
-    return a + b;
-  }
+	// other member functions and variables can go here
+	add(a, b) {
+		return a + b;
+	}
 
-  render() {
-	//jsCode goes here
+	render() {
+		// JS code goes here
+		// but not _all_ your code
 
-
-    return (
-      <div className="App">
-		  <MiniDrawer>
-		  </MiniDrawer>
-      </div>
-    );
-  }
+		return (
+			// JSX / HTML  goes here
+			<div className="App">
+				<MiniDrawer>
+				</MiniDrawer>
+			</div>
+		);
+	}
 }
 
 export default App;
