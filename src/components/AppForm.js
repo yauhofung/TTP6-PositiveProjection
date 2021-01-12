3dc91e66483cbe154c7454e1559b7e29aa820b09
ddesir <d.desir@gmail.com>
2021-01-12 17:09:33 -0500

    Added <Divider /> and adorned text field.

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { Button, TextField, Grid, form } from '@material-ui/core';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(2),
+        width: 400,
+      },
+    },
+  }));
 
 function Form() {
+    const classes = useStyles();
+
     return (
-        <form noValidate autoComplete="off">
-            <TextField id="standard-basic" label="First Name" />
+        <form className={classes.root} direction="row" justify="flex-start" noValidate autoComplete="off">
+            
+            <TextField id="standard-basic" size="medium" label="First Name" />
             <TextField id="standard-basic" label="Last Name" />
+
+            <Divider />
+
+            <TextField
+          label="Annual Salary"
+          id="standard-start-adornment"
+          className={clsx(classes.margin, classes.textField)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
         </form>
     );
 }
 
 function SubmitButton () {
     return (
-        <Button  variant='contained'>Submit</Button>
+        <Button  variant='contained' color="primary">Submit</Button>
     );
 }
 
@@ -20,11 +48,9 @@ class AppForm extends React.Component {
 
     render() {
         return(
-            <Grid container>
-            <Grid item xs={12}>
+            <Grid item xs={3} lg={6}>
                 <Form />
                 <SubmitButton />
-            </Grid>
             </Grid>
         );
     }
